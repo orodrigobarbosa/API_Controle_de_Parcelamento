521a3c7a18fcdfecffa12af645009ccc0285b74c
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-03-26 16:48:45 -0300

    Implementacao Persistencia de Dados

--- src/main/java/com/AppControleParcelamento/AppControleParcelamentoapi/controller/ClienteController.java
+++ src/main/java/com/AppControleParcelamento/AppControleParcelamentoapi/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.AppControleParcelamento.AppControleParcelamentoapi.controller;
 
 import com.AppControleParcelamento.AppControleParcelamentoapi.model.Cliente;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +14,14 @@ import java.util.List;
 @AllArgsConstructor
 
 public class ClienteController {
+
+    @PersistenceContext //instancia a variável manager
+    private EntityManager manager; //USADA PARA FAZER OPERACOES COM AS ENTIDADES DO BANCO DE DADOS.
+
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-   
-        return Arrays.asList();
+    return manager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 
 
--- src/main/java/com/AppControleParcelamento/AppControleParcelamentoapi/model/Cliente.java
+++ src/main/java/com/AppControleParcelamento/AppControleParcelamentoapi/model/Cliente.java
@@ -1,22 +1,30 @@
 package com.AppControleParcelamento.AppControleParcelamentoapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
 
+
+@EqualsAndHashCode (onlyExplicitlyIncluded = true) // Apenas os campos marcados com a anotação @EqualsAndHashCode.Include serão considerados na geração dos métodos equals() e hashCode().
 @Getter
 @Setter
-
+@Entity
 
 public class Cliente {
 
+    @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
